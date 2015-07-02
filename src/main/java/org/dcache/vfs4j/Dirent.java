1eafc0773a158967fd80d8abb09f90c923b7f756
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2015-07-02 10:24:14 +0200

    add missing Dirent.java

---
+++ src/main/java/org/dcache/vfs4j/Dirent.java
@@ -0,0 +1,16 @@
+package org.dcache.vfs4j;
+
+import jnr.ffi.Struct;
+
+public class Dirent extends Struct {
+
+    public Dirent(jnr.ffi.Runtime runtime) {
+        super(runtime);
+    }
+
+    public final Signed64 d_ino = new Signed64();
+    public final Signed64 d_off = new Signed64();
+    public final Unsigned16 d_reclen = new Unsigned16();
+    public final Unsigned8 d_type = new Unsigned8();
+    public final Signed8[] d_name = array(new Signed8[255]);
+}
